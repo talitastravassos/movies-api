9828c262183dc90c3b159aee288a0d34e5d9e463
talitast <talitatravassos.tt@gmail.com>
2019-10-23 18:09:48 -0300

    added search route

--- movie-api/app/Http/Controllers/MoviesController.php
+++ movie-api/app/Http/Controllers/MoviesController.php
@@ -5,19 +5,20 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
 
-// public $client = new Client([
-//     // Base URI is used with relative requests
-//     'base_uri' => 'https://jsonplaceholder.typicode.com/posts',
-//     // You can set any number of default request options.
-//     'timeout'  => 2.0,
-// ]);
-
 class MoviesController extends Controller
 {
 
     public function getUpComing()
     {
         $client = new Client(['base_uri' => 'https://api.themoviedb.org/3/movie/upcoming?api_key=1f54bd990f1cdfb230adb312546d765d&page=1', 'timeout'  => 2.0]);
+        $response = $client->request('GET', '');
+
+        return $response->getBody();
+    }
+
+    public function search($query)
+    {
+        $client = new Client(['base_uri' => 'https://api.themoviedb.org/3/search/movie?api_key=1f54bd990f1cdfb230adb312546d765d&query='.$query.'&page=1', 'timeout'  => 2.0]);
         $response = $client->request('GET', '');
 
         return $response->getBody();
